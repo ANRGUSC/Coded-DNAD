fceed21b8eb7482ea64815aaa4ed5a2e53be528f
Pradipta <iampradiptaghosh@gmail.com>
2018-01-30 15:46:55 -0800

    First Commit

---
+++ scripts/aggregate2.py
@@ -0,0 +1,32 @@
+"""
+ * Copyright (c) 2017, Autonomous Networks Research Group. All rights reserved.
+ *     contributors: 
+ *      Pranav Sakulkar, October 2017
+ *      Jiatong Wang, October 2017
+ *      Aleksandra Knezevic, October 2017
+ *      Bhaskar Krishnamachari, October 2017
+ *     Read license file in main directory for more details  
+"""
+
+import os
+import sys
+import time
+def task(onefile, pathin, pathout):
+
+
+    filelist=[]
+    filelist.append(onefile)
+    time.sleep(15)
+    num=filelist[0].partition('s')[0]
+
+    with open(os.path.join(pathout, num+'merged_file2.ipsum'),'w') as outfile:
+        for filename in filelist:
+            with open(os.path.join(pathin, filename), 'r') as infile:
+                for line in infile:
+                    outfile.write(line)
+
+
+if __name__ == '__main__':
+
+    filelist = ['25split_2']
+    task(filelist, '/home/apac/security_app', '/home/apac/security_app')
